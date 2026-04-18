db270663d24d916b179cc94717e718b7d68e8da7
labubu888 <tanishqabulani@gmail.com>
2026-04-18 12:11:53 +0400

    Add files via upload

---
+++ second.py
@@ -0,0 +1,18 @@
+class employee:
+
+    def __init__(self):
+        print('employee created')
+
+
+    def __del__(self):
+          print("destructor called")
+
+def create_obj():
+     print('making object...')  
+     obj = employee()  
+     print('function end...')  
+     return obj
+
+print('calling Create_obj()function ...')  
+obj = create_obj()  
+print('program end...')  
